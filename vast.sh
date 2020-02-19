cfc5edcc500c61624640f33307f402befe4e1408
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2020-02-18 19:54:21 -0600

    vast

---
+++ vast.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+export vast_tasks="build \
+    lint \
+    v-shfmt \
+    v-bashate \
+    v-shlint \
+    v-checkbashisms \
+    v-shellcheck \
+    v-slick \
+    v-funk \
+    itest"
+
+export vast_origin='build'
